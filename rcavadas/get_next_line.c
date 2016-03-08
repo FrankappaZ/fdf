21cdc5bc57bc54de9319d1b72ba23f22d7c7db77
Renaud CAVADASKI <rcavadas@e2r7p3.42.fr>
2016-03-08 18:53:52 +0100

    Bugs to solve when no Line Feed.

--- rcavadas/get_next_line.c
+++ rcavadas/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rcavadas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/20 19:07:19 by rcavadas          #+#    #+#             */
-/*   Updated: 2016/03/08 14:45:54 by rcavadas         ###   ########.fr       */
+/*   Updated: 2016/03/08 17:51:25 by rcavadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,14 @@ int	treat_extract(char **line, char **extract)
 	index = 0;
 	while (extract[0][index])
 	{
-		if (extract[0][index] == '\n')
+//		ft_putstr("strlen de extract : ");
+//		ft_putnbr(ft_strlen(*extract));
+//		ft_putchar('\n');
+//		ft_putnbr(extract[0][index]);
+//		ft_putchar('\n');
+		if (extract[0][index] == '\n' || extract[0][index] == '\0')
 		{
+//			ft_putendl("COUCOU");
 			*line = ft_strsub(*extract, 0, index);
 			*extract = ft_strsub(*extract, index + 1, ft_strlen(*extract) -
 					ft_strlen(*line));
@@ -46,6 +52,8 @@ int	get_next_line(int const fd, char **line)
 	if (ret > 0)
 	{
 		buf[ret] = '\0';
+		ft_putnbr(buf[ret]);
+		ft_putchar('\n');
 		extract = ft_strjoin(extract, buf);
 	}
 	if (ret == 0)
--- rcavadas/get_next_line.h
+++ rcavadas/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: rcavadas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/20 20:03:04 by rcavadas          #+#    #+#             */
-/*   Updated: 2016/03/08 14:01:50 by rcavadas         ###   ########.fr       */
+/*   Updated: 2016/03/08 17:14:32 by rcavadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 50000
+# define BUFF_SIZE 1000
 # include <fcntl.h>
 # include <stdlib.h>
 # include <sys/types.h>
