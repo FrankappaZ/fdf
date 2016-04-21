628b88b4652f08ed01ebdc644b82cc8381a8927a
Renaud CAVADASKI <rcavadas@e1r7p2.42.fr>
2016-04-21 19:14:19 +0200

    just a few tests using mlx

--- fdf.h
+++ fdf.h
@@ -6,14 +6,20 @@
 /*   By: rcavadas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/20 18:32:27 by rcavadas          #+#    #+#             */
-/*   Updated: 2016/04/21 13:44:04 by rcavadas         ###   ########.fr       */
+/*   Updated: 2016/04/21 18:43:12 by rcavadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 # include <math.h>
+# include <stdio.h>
 # include "libft/includes/libft.h"
 # include "mlx.h"
+
+typedef struct	s_fdf
+{
+	
+}				t_fdf;
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rcavadas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/20 18:30:48 by rcavadas          #+#    #+#             */
-/*   Updated: 2016/04/21 15:41:27 by rcavadas         ###   ########.fr       */
+/*   Updated: 2016/04/21 19:11:52 by rcavadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int main(void)
 {
-	return (0);
+	void	*mlx;
+	void	*win;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 1600, 900, "FdF");
+	mlx_pixel_put(mlx, win, 100, 100, 0x00FFFFFF);
+	mlx_loop(mlx);
 }
